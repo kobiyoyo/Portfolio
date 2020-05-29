da378eeb63215b1699139393cc7851742f1deeb9
kobiyoyo <adamachubi@gmail.com>
2020-05-29 12:32:39 +0100

    added react strap and animation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss';
 
 render(< AppRouter />,document.getElementById('root'))
