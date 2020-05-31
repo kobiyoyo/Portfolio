7a0762767ff77e43fcb8b22728a17a7de7a8e038
kobiyoyo <adamachubi@gmail.com>
2020-05-31 14:29:06 +0100

    finished section two

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,13 @@ const About = () => (
 		 <div className="aboutpage__wrapone"> <img  className="aboutpage__img"  src={require('../images/one.svg')}  alt='one'/></div>
 		 <div className="aboutpage__wraptwo">
 			<p className='aboutpage__one'>I'M Chubi Adama</p> 
-			<p className='aboutpage__two'>A Full-Stack Developer, currently based in Abuja, Nigeria. I'm passionate about creating scalable web experience.</p>
+			<p className='aboutpage__two'>A Full-Stack Developer, currently based in Abuja, Nigeria. I'm passionate about creating scalable web experience for next generation users. </p>
 			<ListGroup>
-		      <ListGroupItem>Cras justo odio</ListGroupItem>
-		      <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-		      <ListGroupItem>Morbi leo risus</ListGroupItem>
-		      <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-		      <ListGroupItem>Vestibulum at eros</ListGroupItem>
+		      <ListGroupItem><b>Current Job:</b> Technical Support Engineer, Microverse</ListGroupItem>
+		      <ListGroupItem><b>Skills:</b> JavaScript (ES5/ES6), Ruby, HTML, CSS, Ruby on Rails, TDD, jQuery, Bootstrap, PostgreSQL, SQLite3, Rspec, Capybara, Git, GitHub, Heroku, React, Webpack, SASS/SCSS </ListGroupItem>
+		      <ListGroupItem><b>Interests:</b> Cooking, Reading, Tennis</ListGroupItem>
+		      <ListGroupItem><b>Education:</b> B.sc Economics, B.sc Peace Studies & Conflict Resolution, Cert. Full-Stack Development</ListGroupItem>
+		      <ListGroupItem><b>Plans:</b> Looking for my next full-time software developer role. I'm also looking forward to learning new technologies and deepening my knowledge of my current skills.</ListGroupItem>
     		</ListGroup>
 		 </div>
 			
